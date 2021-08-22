f24fd32bbcd62b6388435e6f03bfc770729290ed
AhmedHawam9 <ahmed.hawam2030@gmail.com>
2021-08-22 08:26:56 +0200

    push

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
-// start change navbar color
+//============ Animate navbar ============
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
-// $(window).scroll(function () {
-//   $("nav").toggleClass("scrolled", $(this).scrollTop() < 100);
-// });
-
-// End change navbar color
-
-$(document).ready(function () {
-  "use strict";
-
-  $("ul.navbar-nav > li").click(function () {
-    //   e.preventDefault();
-    $("ul.navbar-nav > li").removeClass("active");
-    $(this).addClass("active");
+hamburger.addEventListener("click", () => {
+  //Animate Links
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
   });
+
+  //Hamburger Animation
+  hamburger.classList.toggle("toggle");
 });
+//============ Animate navbar ============
+
+//=========== Active navbar Link in multi pages =============
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll(".nav-links li a");
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].className = "active";
+  }
+}
+//=========== Active navbar Link in multi pages =============
